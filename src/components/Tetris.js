5d2b53b9f4050e29dda00dcdb9c019dcc743e113
ksamarin90 <60395970+ksamarin90@users.noreply.github.com>
2020-04-02 19:04:52 +0300

    New Functions
    Following function were added: statrGame, movePlayer, move, drop, dropPlayer.
    Logic in usePlayer custom hook is updated accordingly.

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -5,6 +5,7 @@ import StartButton from './StartButton';
 
 
 import {StyledTetrisWrapper, StyledTetris} from './styles/StyledTetris';
+import { createStage } from '../gameHelpers';
 import { usePlayer } from '../Hooks/usePlayer';
 import { useStage } from '../Hooks/useStage';
 
@@ -14,13 +15,43 @@ const Tetris = ({ callback }) => {
   const [dropTime, setDropTime] = useState(0);
   const [gameOver, setGameOver] = useState(false);
 
-  const [player] = usePlayer();
-  const [stage, setStage] = useStage();
+  const [player, updatePlayerPos, resetPlayer] = usePlayer();
+  const [stage, setStage] = useStage(player, resetPlayer);
 
   console.log('re-render');
 
+  const startGame = () => {
+    setStage(createStage());
+    resetPlayer();
+  }
+
+  const movePlayer = (dir) => {
+    updatePlayerPos({ x: dir, y: 0 })
+  }
+
+  const move = ({ keyCode }) => {
+    if (!gameOver) {
+      if (keyCode === 37) {
+        movePlayer(-1)
+      } else if (keyCode === 39) {
+        movePlayer(1)
+      } else if (keyCode === 40) {
+        dropPlayer()
+      }
+    }
+  }
+
+  const drop = () => {
+    updatePlayerPos({ x: 0, y: -1, collided: false });
+  }
+
+  const dropPlayer = () => {
+    drop();
+  }
+
+
   return (
-    <StyledTetrisWrapper>
+    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={(e) => move(e)}>
       <StyledTetris>
         <Stage stage={stage} />
           <aside>
@@ -33,7 +64,7 @@ const Tetris = ({ callback }) => {
                 </div>
               )
             }
-            <StartButton />
+            <StartButton onClick={startGame} />
           </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
