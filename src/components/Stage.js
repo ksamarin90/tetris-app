32ce312dd21d9e39997740b216304f602171bb2b
ksamarin90 <60395970+ksamarin90@users.noreply.github.com>
2020-04-02 15:59:11 +0300

    Tetris using React
    General layout and styling for game is completed.

---
+++ src/components/Stage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Cell from './Cell';
+import {StyledStage} from './styles/StyledStage';
+
+const Stage = ({ stage }) => (
+  <StyledStage width={stage[0].length} height={stage.length}>
+    {stage.map(row => row.map((cell, index) => <Cell key={index} type={cell[0]} />))}
+  </StyledStage>
+);
+
+export default Stage;
