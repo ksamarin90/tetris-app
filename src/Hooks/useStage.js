980893de094f981a932f399ac7210e2fe38c9d1d
ksamarin90 <60395970+ksamarin90@users.noreply.github.com>
2020-04-03 15:27:48 +0300

    Clear full row
    If row is completely filled with tetrominos it will be cleared.

--- src/Hooks/useStage.js
+++ src/Hooks/useStage.js
@@ -4,8 +4,21 @@ import { usePlayer } from './usePlayer';
 
 export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage())
+  const [rowsCleared, setRowsCleared] = useState(0)
 
   useEffect(() => {
+    setRowsCleared(0);
+
+    const sweepRows = newStage => 
+      newStage.reduce((acc, row) => {
+        if (row.findIndex(cell => cell[0] === 0) === -1) {
+          setRowsCleared(prev => prev + 1);
+          acc.unshift(new Array(newStage[0].length).fill([0, 'clear']));
+          return acc;
+        }
+        acc.push(row);
+        return acc; 
+      }, [])
 
     const updateStage = (prevStage) => {
 
@@ -25,7 +38,8 @@ export const useStage = (player, resetPlayer) => {
       });
 
       if (player.collided) {
-        resetPlayer()
+        resetPlayer();
+        return sweepRows(newStage)
       }
 
       return newStage
@@ -35,5 +49,5 @@ export const useStage = (player, resetPlayer) => {
 
   }, [player, resetPlayer])
 
-  return [stage, setStage];
+  return [stage, setStage, rowsCleared];
 }
--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -15,7 +15,7 @@ const Tetris = ({ callback }) => {
   const [dropTime, setDropTime] = useState(0);
   const [gameOver, setGameOver] = useState(false);
 
-  const [player, updatePlayerPos, resetPlayer] = usePlayer();
+  const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
   const [stage, setStage] = useStage(player, resetPlayer);
 
   console.log('re-render');
@@ -40,6 +40,8 @@ const Tetris = ({ callback }) => {
         movePlayer(1)
       } else if (keyCode === 40) {
         dropPlayer()
+      } else if (keyCode === 38) {
+        playerRotate(stage, 1)
       }
     }
   }
