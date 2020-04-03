67bbff9d04cbbfe28a4c19b374ee707287274eaf
ksamarin <ksamarin90@gmail.com>
2020-04-03 17:12:07 +0300

    Updates

---
+++ precache-manifest.14c1db325108de910e0345e55bca2f71.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8106119b314a0006b7f67e0b09820aa",
+    "url": "/tetris-app/index.html"
+  },
+  {
+    "revision": "a92c7f0f3a6e30bd0e20",
+    "url": "/tetris-app/static/css/main.6eade72f.chunk.css"
+  },
+  {
+    "revision": "0b25be0dc02fcd6c4317",
+    "url": "/tetris-app/static/js/2.8e480ac2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tetris-app/static/js/2.8e480ac2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a92c7f0f3a6e30bd0e20",
+    "url": "/tetris-app/static/js/main.fed57e42.chunk.js"
+  },
+  {
+    "revision": "2bf1fd2dfb8cc6af3c98",
+    "url": "/tetris-app/static/js/runtime-main.7e0d2f07.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/tetris-app/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/tetris-app/static/media/bg.2810fe6b.png"
+  }
+]);
