ce78cc61346aa67aaebfe9dc3e41c025c9d66835
ksamarin90 <60395970+ksamarin90@users.noreply.github.com>
2020-05-20 13:56:43 +0300

    build for dummy

---
+++ build/precache-manifest.78468a968b3a26f23d15391b4181edb0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba004adb179db2e13cddbe64376aed0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29a7d6ba3e9777b63602",
+    "url": "/static/css/main.e933a24d.chunk.css"
+  },
+  {
+    "revision": "992fe316632fd190f095",
+    "url": "/static/js/2.4d72f121.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4d72f121.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29a7d6ba3e9777b63602",
+    "url": "/static/js/main.fed57e42.chunk.js"
+  },
+  {
+    "revision": "07eaeaa353008e7b5525",
+    "url": "/static/js/runtime-main.5012e7f7.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/static/media/bg.2810fe6b.png"
+  }
+]);
